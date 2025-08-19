e7923191b74f3ebd5b82d00e49bfc9e9d22d2961
demaere-oiie <47918368+demaere-oiie@users.noreply.github.com>
2025-08-19 20:26:40 +0200

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -1,24 +1,33 @@
 const xs = [
 "Anthropic",
+"Chinese Idiom instruction",
 "Facebook",
+"Project idea generator"
 "Stripe",
+"Text-based MUD",
 "The service formerly known as Twitter",
 "Uber",
 ];
 
 const ys = [
 "cats",
+"chess actions",
 "dogs",
 "the Enterprise",
 "LLMs",
+"prose/fiction",
 "vampires",
 ];
 
 const zs = [
 "Assembly",
 "COBOL",
+"Elixir",
+"Gleam",
 "Haskell",
 "JavaScript",
+"LSP server",
+"MCP server",
 "Python",
 ];
 
@@ -26,7 +35,17 @@ const bs = [
 "a 6502",
 "the blockchain",
 "an Nvidia A16",
+"the Rapidriter",
 "a Raspberry Pi",
+];
+
+const cs = [
+"but e2e encrypted",
+"but social",
+"but *very* fast",
+"with a swiping twist",
+"with a IOT twist",
+"with extra AI",
 ];
 
 function sample(list) {
@@ -40,6 +59,8 @@ function main() {
         elem.innerHTML = `${sample(xs)} for ${sample(ys)} in ${sample(zs)}`;
     } else if (template == 1) {
         elem.innerHTML = `${sample(zs)} implemented in ${sample(zs)} on ${sample(bs)}`;
+    } else if (template == 2) {
+        elem.innerHTML = `${sample(xs)} meets ${sample(xs)} ${sample(cs)}`;
     }
 }
 
